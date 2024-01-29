fd9b09eb1d2de6da1a36a01fadf475d63a488003
jpz <rengeyouzh1@163.com>
2024-01-29 23:14:33 +0800

    add newcore

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@ export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/wukong-am
 # export NOOP_HOME=$(pwd)/XiangShan
-# export NOOP_HOME=$(pwd)/NutShell
+export NOOP_HOME=$(pwd)/newcore
